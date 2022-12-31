f8f8805a8d1c0b8ef48e074d3c2ed944ffb430b7
laurac8r <itslaurakaminskiy@gmail.com>
2022-12-31 15:12:39 -0500

    updating form progress bar

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class SignUpForm extends StatefulWidget {
   const SignUpForm();
 
   @override
-  _SignUpFormState createState() => _SignUpFormState();
+  State<SignUpForm> createState() => _SignUpFormState();
 }
 
 class _SignUpFormState extends State<SignUpForm> {
@@ -66,9 +66,29 @@ class _SignUpFormState extends State<SignUpForm> {
     Navigator.of(context).pushNamed('/welcome');
   }
 
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController,
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -110,7 +130,7 @@ class _SignUpFormState extends State<SignUpForm> {
                         : Colors.blue;
                   }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: const Text('Sign up'),
           ),
         ],
